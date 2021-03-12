89e8f9a10773ce2d7c7e8d05b622dbd8c2309032
tsolomitieva <47083801+tsolomitieva@users.noreply.github.com>
2021-03-12 12:48:05 +0200

    Update DayOne.java

--- adventOfCode/src/adventOfCode/DayOne.java
+++ adventOfCode/src/adventOfCode/DayOne.java
@@ -12,10 +12,14 @@ public class DayOne {
 	     while (sc.hasNextLine()) {
 	     inputs.add(sc.nextInt());
 	     
-          }
+         }
 	     int answer=findAnswer(inputs);
-	     System.out.print(answer);
+	     int answer2=findAnswer2(inputs);
+	     System.out.println(answer);
+	     System.out.println(answer2);
 	}
+	
+	
 	     static int findAnswer(List<Integer> inputs) {
 	    	 int num1=0;
 	    	 int num2=0;
@@ -34,6 +38,27 @@ public class DayOne {
 	    		 
 	    	 }
 	    	 
+	     static int findAnswer2(List<Integer> inputs) {
+	    	 int num1=0;
+	    	 int num2=0;
+	    	 int num3=0;
+	    	 for (int i=0; i<inputs.size(); i++) {
+	    		 for (int j=i+1; j<inputs.size(); j++) {
+	    			 for (int k=j+1;k<inputs.size();k++) {
+	    			 if (inputs.get(i)+inputs.get(j)+inputs.get(k)==2020) {
+	    				 num1=inputs.get(i);
+	    				 num2=inputs.get(j);
+	    				 num3=inputs.get(k);
+	    				 
+	    				 break;
+	    			 }
+	    		 }
+	    	    }
+	    	 }
+	    	 return num1*num2*num3;
+			
+	    		 
+	    	 }
 }
      
       
